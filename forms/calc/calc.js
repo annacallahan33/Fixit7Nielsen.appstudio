969122eb3af974eed4a793c7825a0a87c42616f6
benmnielsen <BenNielsen@creighton.edu>
2020-09-30 16:34:45 -0500

    Update calc.js

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -4,5 +4,4 @@ function Calculate(num1, num2) {
 let firstNum = parseInt(prompt("Enter the first number"))
 let secondNum = parseInt(prompt("Enter the second number"))
 let sumNumbers = Calculate(firstNum, secondNum)
-//alert(`The sum of ${firstNum} and ${secondNum} is ${sumNumbers}`) 
-alert(`Hello :)`)
+alert(`The sum of ${firstNum} and ${secondNum} is ${sumNumbers}`) 
